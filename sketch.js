f58f1afe243f56385dbccb2caff266a254549817
LukeDeve <mioacclg@gmail.com>
2025-10-28 17:22:36 +0100

    fine esercitazione in aula

--- sketch.js
+++ sketch.js
@@ -1,17 +1,51 @@
+let data;
+
+
+let minLat, minLon, maxLat, maxLon;
+
+
+
 function preload() {
-  // put preload code here
+  data = loadTable("data-vulcano.csv", "csv", "header");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
-  const message =
-    "This is a template repository\nfor the course Laboratorio di Computergrafica\nCommunication Design, Politecnico di Milano";
-  textAlign(CENTER, CENTER);
-  textSize(16);
-  text(message, width / 2, height / 2);
+
+  //definisco min e max per latitudine e longitudine
+  let allLat = data.getColumn("Latitude");
+  minLat = min(allLat);
+  maxLat = max(allLat);
+
+  let allLon = data.getColumn("Longitude");
+  minLon = min(allLon);
+  maxLon = max(allLon);
+
 }
 
 function draw() {
-  // put drawing code here
+  background(10);
+
+  for(let rowNumber = 0; rowNumber < data.getRowCount(); rowNumber++){
+//leggo i dati di ogni singola riga
+let lat = data.getNum(rowNumber, "Latitude");
+let lon = data.getNum(rowNumber, "Longitude");
+
+//converto le coordinate geografiche in coordinate pixel
+let x = map(lon, minLon, maxLon, 0, width);
+let y = map(lat, minLat, maxLat, height, 0); //invertito perche' latitudine cresce verso nord
+let radius = 20;
+
+
+//calcolare la distanza 
+let d = dist(x, y, mouseX, mouseY);
+if(d < radius){
+  fill("red");
+}
+if (d > radius){
+  fill("green");
+}
+
+ellipse(x, y, radius);
+  }
 }
